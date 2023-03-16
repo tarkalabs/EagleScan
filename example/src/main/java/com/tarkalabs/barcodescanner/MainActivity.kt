abe745b21ffab215f56f48395574953ec03d8506
Kalpesh Patel <1988.kalpesh@gmail.com>
2023-03-16 11:19:42 -0300

    Made MLKit library accessible to library users, So that they can use it to provide barcode formats for scanning.

--- example/src/main/java/com/tarkalabs/barcodescanner/MainActivity.kt
+++ example/src/main/java/com/tarkalabs/barcodescanner/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.tarkalabs.barcodescanner
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.tarkalabs.scanner.models.BarcodeScannerConfig
 import com.tarkalabs.scanner.results.ScanBarcodeContract
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,17 @@ class MainActivity : AppCompatActivity() {
   private val scanResultReceiver = registerForActivityResult(ScanBarcodeContract()) {
     Toast.makeText(this, "result: $it", Toast.LENGTH_SHORT).show()
   }
+  
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     findViewById<Button>(R.id.btn_scan).setOnClickListener {
-      scanResultReceiver.launch(null)
+      scanResultReceiver.launch(
+        BarcodeScannerConfig.Builder()
+          .barcodeFormats(intArrayOf(Barcode.FORMAT_ALL_FORMATS))
+          .showFlashButton(false)
+          .build()
+      )
     }
   }
 }
